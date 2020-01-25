4d356ae91bed5ad8f3b48708ee5cf855a0873234
Ibrahim <antonutkin777@gmail.com>
2020-01-25 10:48:35 +0300

    Добавил скрипт NoJs

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,12 @@
 var menuToggle = document.querySelector(".header__menu-toggle");
 var topMenu = document.querySelector(".header__top-menu");
 var navMenu = document.querySelector(".main-nav__list");
-                
+
+
+menuToggle.classList.remove("header__menu-toggle--nojs");
+topMenu.classList.remove("header__top-menu--nojs");
+navMenu.classList.remove("main-nav__list--nojs");
+
 
 menuToggle.addEventListener("click", function (evt) {
   evt.preventDefault();
