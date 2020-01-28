83a9c517e9a9f27f34eeecedc737f1a5daa812b0
Ibrahim <antonutkin777@gmail.com>
2020-01-29 02:14:29 +0300

    Добавил скрипт для модального окна и выровнял кнопки на IE

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,11 @@
 var menuToggle = document.querySelector(".header__menu-toggle");
 var topMenu = document.querySelector(".header__top-menu");
 var navMenu = document.querySelector(".main-nav__list");
+var formSubmit = document.querySelector(".form-submit__input");
+var modal = document.querySelector(".modal");
+var form = document.querySelector(".form");
+var modalSuccess = document.querySelector(".modal-window--succes");
+var modalFailure = document.querySelector(".modal-window--failure");
 
 menuToggle.classList.remove("header__menu-toggle--nojs");
 menuToggle.classList.remove("header__top-menu--overlap");
@@ -13,4 +18,23 @@ menuToggle.addEventListener("click", function (evt) {
   menuToggle.classList.toggle("header__menu-toggle--opened");
   topMenu.classList.toggle("header__top-menu--opened");
   navMenu.classList.toggle("main-nav__list--opened");
+});
+
+formSubmit.addEventListener("click", function (evt) {
+  evt.preventDefault();  
+  if (form.checkValidity() === true) {
+    modalSuccess.classList.add("modal-window--opened");
+    modalFailure.classList.remove("modal-window--opened");
+  } else {
+    modalFailure.classList.add("modal-window--opened");
+    modalSuccess.classList.remove("modal-window--opened");
+  }
+  modal.classList.add("modal--show");
+});
+
+modal.addEventListener("click", function (evt) {
+  var target = evt.target;  
+  if (target.classList.contains("modal__close")) {
+    modal.classList.remove("modal--show");
+  }
 });
